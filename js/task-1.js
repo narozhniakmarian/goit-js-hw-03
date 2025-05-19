54ac286d15b565f6c219d1a78dafe87a0d4a6a71
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-05-19 09:30:22 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,11 @@
+function slugify(title) {
+	const trimTitle = title.toLowerCase().trim();
+	const replayTitle = trimTitle.replaceAll(" ", "-");
+	return replayTitle;
+}
+console.log(slugify("    Arrays for beginners "));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
 
